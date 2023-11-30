5e81cc650373db9c891f9669327f770b350de758
Lingshu Hu <lingshu.hu@hotmail.com>
2023-11-29 22:02:08 -0500

    update

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ BATCH_SIZE = 512
 SVM_LSTM_MODEL_PATH = 'trained_models/step2_model_svm_200tweets_lstm.sav'
 SVM_BERT_MODEL_PATH = 'trained_models/step2_model_svm_200tweets_bert.sav'
 
-DATA_PATH = "data/election_us_20k_user_independents.csv"
-TWEET_PRED_DATA_PATH = "pred_data/election_us_20k_user_independents_pred.csv"
-USER_PRED_DATA_PATH = "pred_data/election_us_20k_user_independents_user_pred.csv"
+DATA_PATH = "data/example_tweets.csv"
+TWEET_PRED_DATA_PATH = "pred_data/example_tweets_pred.csv"
+USER_PRED_DATA_PATH = "pred_data/example_tweets_user_pred.csv"
 
 TEXT_COLUMN_NAME = "text"
 USER_COLUMN_NAME = "user_id"
